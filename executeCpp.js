a33a07d2fc5351b34d5e673d761abd4029ed2bc6
animesh-0041 <ghoroianimesh1508@gmail.com>
2023-11-06 12:25:51 +0530

    dne

--- executeCpp.js
+++ executeCpp.js
@@ -1,29 +1,29 @@
-const { exec } = require("child_process");
-const fs = require("fs");
-const path = require("path");
+// const { exec } = require("child_process");
+// const fs = require("fs");
+// const path = require("path");
 
-const outputPath = path.join(__dirname, "outputs");
+// const outputPath = path.join(__dirname, "outputs");
 
-if (!fs.existsSync(outputPath)) {
-  fs.mkdirSync(outputPath, { recursive: true });
-}
+// if (!fs.existsSync(outputPath)) {
+//   fs.mkdirSync(outputPath, { recursive: true });
+// }
 
-const executeCpp = (filepath) => {
-  const jobId = path.basename(filepath).split(".")[0];
-  const outPath = path.join(outputPath, `${jobId}.out`);
+// const executeCpp = (filepath) => {
+//   const jobId = path.basename(filepath).split(".")[0];
+//   const outPath = path.join(outputPath, `${jobId}.out`);
 
-  return new Promise((resolve, reject) => {
-    exec(
-      `g++ ${filepath} -o ${outPath} && cd ${outputPath} && ./${jobId}.out`,
-      (error, stdout, stderr) => {
-        error && reject({ error, stderr });
-        stderr && reject(stderr);
-        resolve(stdout);
-      }
-    );
-  });
-};
+//   return new Promise((resolve, reject) => {
+//     exec(
+//       `g++ ${filepath} -o ${outPath} && cd ${outputPath} && ./${jobId}.out`,
+//       (error, stdout, stderr) => {
+//         error && reject({ error, stderr });
+//         stderr && reject(stderr);
+//         resolve(stdout);
+//       }
+//     );
+//   });
+// };
 
-module.exports = {
-  executeCpp
-}
+// module.exports = {
+//   executeCpp
+// }
